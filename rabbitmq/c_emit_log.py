d68fac8dc5c92e76d8f4c1fb5e56561d1cfc00cd
spadeninja <quanshuli.dev@gmail.com>
2021-10-12 23:43:42 +1000

    update c

---
+++ rabbitmq/c_emit_log.py
@@ -0,0 +1,19 @@
+import pika, sys
+#from pika import exchange_type
+
+# broker: localhost
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+# exchange types: direct, topic, headers and fanout
+channel.exchange_declare(exchange='logs',
+                         exchange_type='fanout')
+
+message = ' '.join(sys.argv[1:]) or 'info: Hello World!'
+channel.basic_publish(exchange='logs',
+                      routing_key='',
+                      body=message
+                      )
+print(' [x] Sent %r ' % message)
+
+connection.close()
