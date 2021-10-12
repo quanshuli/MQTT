1890f9d29c92143c9230db796bc77907bf52031a
spadeninja <quanshuli.dev@gmail.com>
2021-10-12 22:10:28 +1000

    Update subscribe.py

--- subscribe.py
+++ subscribe.py
@@ -5,11 +5,7 @@ import paho.mqtt.client as mqtt
 import time
 import json
 
-#print('1')
 def on_message(client, userdata, message):
-    #print('received: ', m_decode)
-    #print('2')
-    #print('received: ', str(message.payload.decode('utf-8')))
     m_decode = str(message.payload.decode('utf-8'))
     m_json = json.loads(m_decode)
     print(m_json)
@@ -22,7 +18,7 @@ client.connect(mqttBroker)
 client.loop_start()
 
 client.subscribe('RANDOM_NUMBER')
-#print('3')
+
 client.on_message=on_message
 
 time.sleep(30)
