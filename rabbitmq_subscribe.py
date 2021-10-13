e14092fa9e06a72764c0920a8e69c2a841032a96
spadeninja <quanshuli.dev@gmail.com>
2021-10-13 13:47:22 +1000

    Update rabbitmq_subscribe.py

--- rabbitmq_subscribe.py
+++ rabbitmq_subscribe.py
@@ -3,9 +3,12 @@ mqtt through rabbitmq
 '''
 import pika, sys, json, time
 
-from pika import exchange_type
-from mqtt_publish import mqttPassword, mqttUser, mqttBrokerHost, \
-                          mqttTopic
+mqttBrokerHost = '127.0.0.1'
+mqttBrokerPort = 1883
+mqttUser = 'guest'
+mqttPassword = 'guest'
+mqttTopic = 'generated_numbers'
+mqttClientId = 'Numbers'
 
 # set up the credentials same as mqtt publisher
 user_pwd = pika.PlainCredentials(mqttUser, mqttPassword)
@@ -17,7 +20,8 @@ exchangeType = 'topic'
 # set up connections in rabbitmq
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(
-        mqttBrokerHost, credentials=user_pwd
+        host=mqttBrokerHost, 
+        #credentials=user_pwd
     ))
 channel = connection.channel()
 
@@ -27,24 +31,22 @@ channel.exchange_declare(
 )
 
 # declare queue, delete the queue when consumer quits
-result = channel.queue_declare(exclusive=True)
-
-# random queue name
+result = channel.queue_declare(queue='', exclusive=True)
 queue_name = result.method.queue
 
 # binding keys
-binding_keys = [mqttTopic]
+binding_key = mqttTopic
 
 # binding queues
 channel.queue_bind(
-    exchange=exchangeTopic, queue=queue_name, routing_key=binding_keys)
+    exchange=exchangeTopic, queue=queue_name, routing_key=binding_key)
 
 # set up callback
 def callback(ch, method, properties, body):
     print(' [x] Received %r' % body.decode())
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+    #ch.basic_ack(delivery_tag=method.delivery_tag)
 
-channel.basic_qos(prefetch_count=1)
+#channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue=queue_name,
                       # auto_ack: redeliver if not consumed
                       # auto_ack=True, # no message acknowledgment with True
